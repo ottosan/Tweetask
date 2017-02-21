112acb6fa13cf2b4b888432941641c1bbb6995be
音石朋恵 <tomoe_otoishi@Tomoe-Otoishi.local>
2017-02-21 21:10:54 +0900

    add twitter

--- Podfile
+++ Podfile
@@ -3,5 +3,8 @@ use_frameworks!
 
 target 'Tweetask' do
   pod 'RAMAnimatedTabBarController', '~> 2.0.13'
+  pod 'RealmSwift', '2.1.2'
+  pod 'Firebase/Auth'
+  pod 'TwitterKit'
 end
 
---
+++ Tweetask/LoginViewController.swift
@@ -0,0 +1,73 @@
+//
+//  LoginViewController.swift
+//  Tweetask
+//
+//  Created by 音石朋恵 on 2017/02/21.
+//  Copyright © 2017年 tomoe.otoishi. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import TwitterKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func setup(){
+//        let logInButton = TWTRLogInButton(logInCompletion: { session, error in
+//            if (session != nil) {
+//                print("signed in as \(session?.userName)");
+//            } else {
+//                print("error: \(error?.localizedDescription)");
+//            }
+//        })
+//        logInButton.center = self.view.center
+//        self.view.addSubview(logInButton)
+        let logInButton = TWTRLogInButton(logInCompletion: { session, error in
+            
+            if let session = session {
+                
+                let credential = FIRTwitterAuthProvider.credential(withToken: session.authToken,
+                                                                            secret: session.authTokenSecret)
+                
+                FIRAuth.auth()?.signIn(with: credential) { (user, error) in
+                    
+                    if let error = error {
+                        //TOOD: エラーハンドリング
+                        print(error)
+                        return
+                    }
+                    
+                    print("ようこそ! \(user?.displayName)")
+                }
+                
+            } else {
+                //TOOD: エラーハンドリング
+            }
+        })
+        
+        logInButton.center = view.center
+        self.view.addSubview(logInButton)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
